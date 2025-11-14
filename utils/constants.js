40fa8ff502c150aa616cc198ebd62798ae8a64ba
James Chang <james.chang@igoodworks.com>
2025-11-13 23:22:01 -0800

    fix baseUrl for WebApp

--- utils/constants.js
+++ utils/constants.js
@@ -2,3 +2,5 @@ export const baseUrl =
   process.env.UNIDIR_API_BASE || "http://oauth.biocloud.pro/v1";
 
 export const baseUrlAuth = process.env.UNIDIR_OAUTH_API_BASE;
+
+export const baseUrlWebApp = process.env.UNIDIR_WEB_APP_BASE;
